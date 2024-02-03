b6e07b3556a4248a8236545c3c3593f91fe58a80
sudeera-madushan <sudeeramadushan888@gmail.com>
2024-02-03 10:29:15 +0530

    get all users success

--- src/view/createUser.tsx
+++ src/view/createUser.tsx
@@ -9,9 +9,9 @@ const CreateUser = ():JSX.Element => {
     const API_URL = 'https://8ea71269743349ec9437d28d1cd0278f.weavy.io';
     const API_KEY = 'wys_adTKKp1VrXPVQGp1x5rgd16Rgw9f7A3e3HKF';
     const userData = {
-        uid: 'user-1',
-        name: 'John Doe',
-        directory: 'acme'
+        uid: 'user-3',
+        name: 'Sudeera',
+        directory: 'test'
     };
 
     const axiosConfig = {
@@ -33,10 +33,20 @@ const CreateUser = ():JSX.Element => {
                 console.error('Error:', error);
             });
     }
+    const getAllUser= () => {
+        axios.get(API_URL+"/api/users", axiosConfig)
+            .then(response => {
+                console.log('Success:', response.data);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    }
 
     return (
         <h1>
             <button onClick={createUser}>Create</button>
+            <button onClick={getAllUser}>Get All</button>
         </h1>
     )
 }
